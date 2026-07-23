77a99b7409372b1a6e63bbad8c612fade814316a
prakash khadka <Prakashkhadka0003@gmail.com>
2026-07-23 07:05:10 +0545

    Update

---
+++ Take two number and Print their sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main () {
+    int num1 ,num2 ,sum;
+
+    Printf("Enter the first number:");
+    scanf("%d",&num1);
+
+    Printf("Enter the Second number:");
+    scanf("%d",&num2);
+
+    sum=num1=num2
+
+    Printf("Sum=%d\n",sum);
+
+    return 0;
+}
