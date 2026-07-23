0d92b7656c5725f811f4e289cfb5b40087a44feb
prakash khadka <Prakashkhadka0003@gmail.com>
2026-07-23 07:16:02 +0545

    Update

--- Take three number and Print their sum.c
+++ Take three number and Print their sum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2, num3, sum;
 
     printf("Enter the first number:");
     scanf("%d",&num1);
@@ -9,7 +9,11 @@ int main() {
     printf("Enter the Second number:");
     scanf("%d",&num2);
 
-    sum=num1+num2;
+    printf("Enter the third number:");
+    scanf("%d",&num3);
+
+
+    sum=num1+num2=num3;
 
     printf("Sum=%d\n",sum);
 
