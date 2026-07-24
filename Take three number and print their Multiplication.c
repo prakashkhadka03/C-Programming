cf3a76eecd133e3b5a5eba5d7a24fcf8d591aa75
prakash khadka <Prakashkhadka0003@gmail.com>
2026-07-24 06:39:42 +0545

    update

---
+++ Take three number and print their Multiplication.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3, product;
+
+    printf("Enter the first number:");
+    scanf("%d",&num1);
+
+    printf("Enter the Second number:");
+    scanf("%d",&num2);
+
+    printf("Enter the third number:");
+    scanf("%d",&num3);
+
+
+    Product=num1*num2*num3;
+
+    printf("Product=%d\n",product);
+
+    return 0;
+}
---
+++ Take two number and print their  difference.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, difference;
+
+    printf("Enter the first number:");
+    scanf("%d",&num1);
+
+    printf("Enter the Second number:");
+    scanf("%d",&num2);
+
+    differnce=num1-num2;
+
+    printf("Difference=%d\n",difference);
+
+    return 0;
+}
