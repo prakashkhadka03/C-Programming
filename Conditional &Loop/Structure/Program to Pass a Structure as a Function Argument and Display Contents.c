f28598294c9eed83f48c8ef14609c3ad4db57ad6
prakash khadka <Prakashkhadka0003@gmail.com>
2026-07-31 10:57:19 +0545

    Final

---
+++ Conditional &Loop/Structure/Program to Pass a Structure as a Function Argument and Display Contents.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+void display(struct Student s) {
+
+    printf("\nStudent Information\n");
+    printf("Roll Number: %d\n",s.roll);
+    printf("Name: %s\n",s.name);
+    printf("Marks: %.2f\n",s.marks);
+}
+
+
+int main() {
+
+    struct Student s;
+
+    printf("Enter Roll Number: ");
+    scanf("%d",&s.roll);
+
+    printf("Enter Name: ");
+    scanf("%s",s.name);
+
+    printf("Enter Marks: ");
+    scanf("%f",&s.marks);
+
+
+    display(s);
+
+    return 0;
+}
