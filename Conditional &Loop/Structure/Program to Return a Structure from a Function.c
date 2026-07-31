9198f5643e8ed04d13f54462b3fbbdae1d1586ca
prakash khadka <Prakashkhadka0003@gmail.com>
2026-07-31 12:17:26 +0545

    Final

---
+++ Conditional &Loop/Structure/Program to Return a Structure from a Function.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+struct Student {
+
+    int roll;
+    char name[50];
+    float marks;
+
+};
+
+
+struct Student getStudent() {
+
+    struct Student s;
+
+    printf("Enter Roll Number: ");
+    scanf("%d",&s.roll);
+
+    printf("Enter Name: ");
+    scanf("%s",s.name);
+
+    printf("Enter Marks: ");
+    scanf("%f",&s.marks);
+
+    return s;
+}
+
+
+int main() {
+
+    struct Student s;
+
+    s = getStudent();
+
+
+    printf("\nStudent Details\n");
+    printf("Roll Number: %d\n",s.roll);
+    printf("Name: %s\n",s.name);
+    printf("Marks: %.2f\n",s.marks);
+
+
+    return 0;
+}
